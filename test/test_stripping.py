7546a5152777dea7e8a027cce0f97f8ec9fe18f8
harrisonpim <harrison.stroma@gmail.com>
2021-08-14 18:17:58 +0100

    add tests for stripping lines

---
+++ test/test_stripping.py
@@ -0,0 +1,22 @@
+from dotenv_stripout.stripout import strip_line
+
+
+def test_normal_line():
+    input_line = "MY_SECRET_PASSWORD=IqLTLrFviwHTDKWGZoR7uB2JtM1wjwE34MBwoztE"
+    expected_output_line = "MY_SECRET_PASSWORD=\n"
+    output_line = strip_line(input_line)
+    assert output_line == expected_output_line
+
+
+def test_line_without_value():
+    input_line = "MY_SECRET_PASSWORD="
+    expected_output_line = "MY_SECRET_PASSWORD=\n"
+    output_line = strip_line(input_line)
+    assert output_line == expected_output_line
+
+
+def test_line_without_equals():
+    input_line = "MY_SECRET_PASSWORD"
+    expected_output_line = "MY_SECRET_PASSWORD=\n"
+    output_line = strip_line(input_line)
+    assert output_line == expected_output_line
