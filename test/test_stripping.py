c25bbcd36793b77601694afb340ac0a76cbe6789
harrisonpim <harrison.stroma@gmail.com>
2021-08-19 19:19:42 +0100

    update strip tests

--- test/test_stripping.py
+++ test/test_stripping.py
@@ -3,20 +3,20 @@ from dotenv_stripout.stripout import strip_line
 
 def test_normal_line():
     input_line = "MY_SECRET_PASSWORD=IqLTLrFviwHTDKWGZoR7uB2JtM1wjwE34MBwoztE"
-    expected_output_line = "MY_SECRET_PASSWORD=\n"
+    expected_output_line = "MY_SECRET_PASSWORD="
     output_line = strip_line(input_line)
     assert output_line == expected_output_line
 
 
 def test_line_without_value():
     input_line = "MY_SECRET_PASSWORD="
-    expected_output_line = "MY_SECRET_PASSWORD=\n"
+    expected_output_line = "MY_SECRET_PASSWORD="
     output_line = strip_line(input_line)
     assert output_line == expected_output_line
 
 
 def test_line_without_equals():
     input_line = "MY_SECRET_PASSWORD"
-    expected_output_line = "MY_SECRET_PASSWORD=\n"
+    expected_output_line = "MY_SECRET_PASSWORD="
     output_line = strip_line(input_line)
     assert output_line == expected_output_line
